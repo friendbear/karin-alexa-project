61ad897f7c399bbef3705bb9e10e0686a6f363f4
T Kumagai <s8zmnonun@relay.firefox.com>
2021-11-20 14:54:13 +0900

    add log

--- hello-karin.py
+++ hello-karin.py
@@ -1,9 +1,13 @@
+import logging
+import os
+
 from flask import Flask, render_template
-from flask_ask import Ask, statement, question
+from flask_ask import Ask, request, session, question, statement
 
 app = Flask(__name__)
 
 ask = Ask(app, '/')
+logging.getLogger('flask_ask').setLevel(logging.DEBUG)
 
 @ask.launch
 def launch():
